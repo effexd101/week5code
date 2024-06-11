1504ed25724762ec4290540f36786cfdb8d97e04
effexd101 <168129228+effexd101@users.noreply.github.com>
2024-06-11 19:30:39 -0400

    Update main.tf

--- Code5/main.tf
+++ Code5/main.tf
@@ -13,7 +13,7 @@ resource "aws_lightsail_instance" "custom2" {
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
-  key_pair_name     = "Week2"
+  key_pair_name     = ""
   user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo tee /var/www/html/index.html"
   tags = {
     Team      = "DevOps"
